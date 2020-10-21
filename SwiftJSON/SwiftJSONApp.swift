fa38187668381427c6c6820b7822735f983d89c0
Jan Hovland <jan.hovland@lyse.net>
2020-10-21 09:48:11 +0200

    Initial Commit

---
+++ SwiftJSON/SwiftJSONApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftJSONApp.swift
+//  SwiftJSON
+//
+//  Created by Jan Hovland on 21/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct SwiftJSONApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
