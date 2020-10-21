8b9372211f8e7d375c97bb8f5c25f84162539eca
Jan Hovland <jan.hovland@lyse.net>
2020-10-21 20:51:55 +0200

    Update

--- SwiftJSON/ContentView.swift
+++ SwiftJSON/ContentView.swift
@@ -4,41 +4,43 @@ import SwiftUI
 
 // MARK: - WelcomeDetailElement
 struct PersonElement: Codable, Identifiable {
-        let id: Int
-        let personData: PersonData
+    let id: Int
+    let personData: PersonData
 }
 
 // MARK: - PersonData
 struct PersonData:Codable {
-        let address: String
-        let city: String
-        let name: String
+    let address: String
+    let city: String
+    let firstName: String
+    let lastName: String
+    let name: String
 }
 
 
 struct ContentView: View {
-
+    
     @State var result : [PersonElement] = []
-
+    
     var body: some View{
         List(result) {a in
-            Text(a.personData.name)
+            Text(a.personData.firstName + " " + a.personData.lastName)
         }
         .onAppear(perform: parseJson)
     }
-
+    
     func parseJson() {
-
+        
         if let url = Bundle.main.url(forResource: "Data.json", withExtension: nil){
-
+            
             if let data = try? Data(contentsOf: url){
-
+                
                 let jsondecoder = JSONDecoder()
-
+                
                 do{
                     let result = try jsondecoder.decode([PersonElement].self, from: data)
                     self.result = result
-
+                    
                 }
                 catch {
                     print("error trying parse json")
